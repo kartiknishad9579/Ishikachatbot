f7fbb80aadb7b6e27048f33e3d3a61e16cbe3beb
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-16 02:36:48 +0530

    Update config.py

--- Ishikachat/config.py
+++ Ishikachat/config.py
@@ -6,7 +6,9 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "12345"))
 API_HASH = os.getenv("API_HASH", "your_api_hash")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-MONGO_DB_URI = os.getenv("MONGO_DB_URI", "mongodb://localhost:27017")
+
+# Yaha direct Mongo URL daal diya
+MONGO_DB_URI = "mongodb+srv://KartikNishad:kartikNishad@ishika.jgzfxou.mongodb.net/IshikaDB?retryWrites=true&w=majority&appName=Ishika"
 
 BOT_NAME = "Ishika"
 BOT_USERNAME = os.getenv("BOT_USERNAME", "Ishika_chat_bot")
