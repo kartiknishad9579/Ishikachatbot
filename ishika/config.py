639dbc7e9068ddeb25276d4f35915c6d67cce37e
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-16 17:53:33 +0530

    Update config.py

--- ishika/config.py
+++ ishika/config.py
@@ -7,6 +7,7 @@ API_ID = int(os.getenv("API_ID", "12345"))
 API_HASH = os.getenv("API_HASH", "your_api_hash")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
 MONGO_DB_URI = os.getenv("MONGO_DB_URI", "mongodb://localhost:27017")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 
 BOT_NAME = "Ishika"
 BOT_USERNAME = os.getenv("BOT_USERNAME", "Ishika_chat_bot")
