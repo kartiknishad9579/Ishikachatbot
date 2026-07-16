d1328e9937b122e09de24fe58a3fc430e27f865c
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-16 17:54:31 +0530

    Update __init__.py

--- ishika/__init__.py
+++ ishika/__init__.py
@@ -1,12 +1,21 @@
-import asyncio
-from ishika import app, LOGGER
-from ishika.modules.helpers.mongo import mongoping
+import logging
 
-async def main():
-    await app.start()
-    await mongoping()
-    LOGGER.info(f"IshikaChatBot Started Successfully ✅")
-    await asyncio.Event().wait()
+from pyrogram import Client
 
-if __name__ == "__main__":
-    asyncio.run(main())
+from ishika.config import API_ID, API_HASH, BOT_TOKEN
+
+logging.basicConfig(
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    level=logging.INFO,
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+LOGGER = logging.getLogger("Ishika")
+
+app = Client(
+    "IshikaBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="ishika/modules"),
+)
