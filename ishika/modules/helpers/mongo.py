deb621a1495d623b88eefc24438380950292be76
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-17 13:05:54 +0530

    Update mongo.py

--- ishika/modules/helpers/mongo.py
+++ ishika/modules/helpers/mongo.py
@@ -3,7 +3,7 @@ from pymongo.errors import ServerSelectionTimeoutError
 from ishika.config import MONGO_DB_URI, LOGGER
 
 _client = AsyncIOMotorClient(MONGO_DB_URI, serverSelectionTimeoutMS=5000)
-IshikaDB = _client["IshikaDB"] # <- naam same rakha jo URI me hai
+IshikaDB = _client["IshikaDB"]
 
 afk = IshikaDB.afk
 notes = IshikaDB.notes
@@ -11,10 +11,9 @@ filters = IshikaDB.filters
 welcome = IshikaDB.welcome
 couples = IshikaDB.couples
 
-
 async def mongoping():
     try:
-        await _client.admin.command("ping") # <- await lag gaya
+        await _client.admin.command("ping")
         LOGGER.info("[Ishika] MongoDB Connected ✅")
     except ServerSelectionTimeoutError as e:
         LOGGER.error(f"[Ishika] MongoDB Connection Failed ❌ -> {e}")
