56284751c1c840b496b2f817915928f573d7b6f5
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-17 15:17:29 +0530

    Update __init__.py

--- ishika/__init__.py
+++ ishika/__init__.py
@@ -1,1 +1,9 @@
+from pyrogram import Client
+from ishika.config import API_ID, API_HASH, BOT_TOKEN
 
+app = Client(
+    "IshikaBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
