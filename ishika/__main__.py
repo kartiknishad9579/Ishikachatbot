05c6a83113f35e9bedca8f907dc9a53716c8babc
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-17 13:15:41 +0530

    Update __main__.py

--- ishika/__main__.py
+++ ishika/__main__.py
@@ -1,4 +1,6 @@
 import asyncio
+import glob
+import importlib
 from pyrogram import Client
 from ishika.modules.helpers.mongo import mongoping
 from ishika.config import API_ID, API_HASH, BOT_TOKEN, LOGGER
@@ -10,9 +12,22 @@ app = Client(
     bot_token=BOT_TOKEN
 )
 
+async def load_modules():
+    """Sabhi modules auto load karega"""
+    path = "ishika/modules/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as a:
+            pxt = a.read()
+            if "__mod_name__" in pxt:
+                module_name = name.split("\\")[-1].replace(".py", "")
+                imported_module = importlib.import_module(f"ishika.modules.{module_name}")
+                LOGGER.info(f"[Ishika] Loaded module: {imported_module.__mod_name__}")
+
 async def main():
     await mongoping()
     await app.start()
+    await load_modules() # <- ye line add ki
     me = await app.get_me()
     LOGGER.info(f"[Ishika] Bot logged in as @{me.username} ✅")
     LOGGER.info("[Ishika] IshikaChatBot Started Successfully ✅")
