4b706d708c4cdae2f961e71c93af5c1709c74989
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-18 00:14:34 +0530

    Delete ishika/owner.py

--- ishika/owner.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import filters
-from pyrogram.types import Message
-from ishika import app
-from ishika.config import OWNER_ID
-
-@app.on_message(filters.user(OWNER_ID) & filters.command("owner"))
-async def owner_panel(_, message: Message):
-    await message.reply("Owner Panel: Bot is running ✅")
