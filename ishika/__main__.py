9dda2515553c7296762032c8136ce6c28aaf0c0f
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-17 13:31:07 +0530

    Update __main__.py

--- ishika/__main__.py
+++ ishika/__main__.py
@@ -1,16 +1,20 @@
 import asyncio
-import glob
+import os
+import importlib
 from ishika import app
 from ishika.modules.helpers.mongo import mongoping
 from ishika.config import LOGGER
 
 async def load_modules():
-    path = "ishika/modules/*.py"
-    files = glob.glob(path)
-    for name in files:
-        module_name = name.replace("ishika/modules/", "").replace(".py", "")
-        __import__(f"ishika.modules.{module_name}")
-        LOGGER.info(f"[Ishika] Loaded module: {module_name}")
+    path = "ishika/modules"
+    for filename in os.listdir(path):
+        if filename.endswith(".py") and not filename.startswith("_"):
+            module_name = f"ishika.modules.{filename[:-3]}"
+            try:
+                importlib.import_module(module_name)
+                LOGGER.info(f"[Ishika] Loaded module: {filename[:-3]}")
+            except Exception as e:
+                LOGGER.error(f"[Ishika] Failed to load {filename}: {e}")
 
 async def main():
     await mongoping()
