c1f5272ddef1cde80e7f299add4c6ae2a86a679f
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-17 16:28:39 +0530

    Update start.py

--- ishika/modules/start.py
+++ ishika/modules/start.py
@@ -1,21 +1,29 @@
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from ishika import app
-from ishika.modules.helpers.inline import start_panel
 
 BOT_NAME = "Ishika"
+
+def start_panel():
+    return InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton("➕ Add Me", url="https://t.me/IshikaBot?startgroup=true"),
+            InlineKeyboardButton("💭 Support", url="https://t.me/Ishika_Support")
+        ],
+        [InlineKeyboardButton("💬 Channel", url="https://t.me/Ishika_Updates")]
+    ])
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message: Message):
     await message.reply_photo(
-        photo="https://telegra.ph/file/placeholder.jpg", # <-- yaha apni image ka link daal
-        caption=f"**Hey {message.from_user.mention}**\n\n"
+        photo="https://graph.org/file/c2d2b7beeb5d3a8b2b1a1.jpg",
+        caption=f"**Hey {message.from_user.mention}**\n"
                 f"**I am {BOT_NAME}** ✨\n"
-                f"**Advanced Group Manager Bot**\n\n"
+                f"**Advanced Group Manager Bot**\n"
                 f"**Press below buttons to explore**",
         reply_markup=start_panel()
     )
 
 @app.on_message(filters.command("start") & filters.group)
 async def start_group(client, message: Message):
-    await message.reply(f"**{BOT_NAME} is Online** ✅\n**I am alive and working**")
+    await message.reply(f"**{BOT_NAME} is Online** ✅")
