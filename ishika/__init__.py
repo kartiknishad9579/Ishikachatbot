ed9ae0a1fe7597a686a2026cb7775d039f257d23
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-17 22:24:12 +0530

    Update __init__.py

--- ishika/__init__.py
+++ ishika/__init__.py
@@ -1,9 +1,16 @@
 from pyrogram import Client
 from ishika.config import API_ID, API_HASH, BOT_TOKEN
+from ishika.config import LOGGER
 
 app = Client(
     "IshikaBot",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    bot_token=BOT_TOKEN,
+    workers=32
 )
+
+from ishika import handlers 
+
+LOGGER.info("[Ishika] Starting Bot...")
+app.run()
