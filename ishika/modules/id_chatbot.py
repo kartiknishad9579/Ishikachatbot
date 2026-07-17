643bf4c26a407986c1f549bd3b1b1c2be545cdfc
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-18 01:43:11 +0530

    Update id_chatbot.py

--- ishika/modules/id_chatbot.py
+++ ishika/modules/id_chatbot.py
@@ -1,18 +1,18 @@
 import asyncio
 import random
 from pyrogram import filters
-from ishika import app, userbot
-from ishika.config import OWNER_ID, LOGGER
+from ishika import userbot
+from ishika.config import LOGGER
 
 if userbot:
-    @userbot.on_message(filters.text & filters.incoming & ~filters.bot & ~filters.me)
+    @userbot.on_message(filters.reply & filters.incoming & ~filters.bot & ~filters.me)
     async def id_chatbot_reply(client, message):
         me = await client.get_me()
-        if message.reply_to_message and message.reply_to_message.from_user.id == me.id:
+        if message.reply_to_message.from_user.id == me.id:
             replies = ["Haan bolo? 💜", "Main Ishika hu", "Kya hua?"]
-            await asyncio.sleep(1.5)
+            await asyncio.sleep(2)
             await message.reply(random.choice(replies))
             LOGGER.info(f"ID Chatbot replied in {message.chat.id}")
 
-async def restart_idchatbots():
+def restart_idchatbots():
     LOGGER.info("ID Chatbot module loaded")
